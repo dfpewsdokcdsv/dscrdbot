af2837c21a96b703fc6b556e519abc40bf472dd2
dfpewsdokcdsv <132742180+dfpewsdokcdsv@users.noreply.github.com>
2023-06-10 12:11:20 +0600

    Update dscrdbot.py

--- dscrdbot.py
+++ dscrdbot.py
@@ -3,12 +3,7 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 bot = commands.Bot(command_prefix='$', intents=intents)
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
